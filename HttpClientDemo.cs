9ebaa1b7de049945f61ae2f3cd7f8f1714d58fb9
vinyvk <61677106+vinyvk@users.noreply.github.com>
2020-04-16 18:54:57 +0530

    To fetch the data from other API

---
+++ HttpClientDemo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace httpClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Fetching Data from one API to another API
+            using HttpClient c = new HttpClient();
+            c.BaseAddress=new Uri("https://localhost:44383/api/Students/");
+            HttpResponseMessage responseMessage=await c.GetAsync(c.BaseAddress);
+            string str=await c.GetStringAsync(c.BaseAddress);
+            Console.WriteLine("Status:"+responseMessage.StatusCode);
+            Console.WriteLine("Response Data:"+str);
+        }
+    }
+}
